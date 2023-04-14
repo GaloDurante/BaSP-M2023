78f03a55d6ee317163e63008ac318a0cdbdadc1e
Galo Durante <durantegalo@gmail.com>
2023-04-14 10:39:20 -0300

    BaSP-W05-2023: create the folders and solve exercise 1

---
+++ Week-05/exercises/01-variables-and-operators.js
@@ -0,0 +1,15 @@
+console.log('Ejercicio 1');
+console.log('1-a:');
+var num1 = 5, num2 = 10;
+var resultado = num1 + num2;
+console.log('La suma de los numeros es: ',resultado);
+
+console.log('1-b:');
+var cadena = 'Messi', cadena2 = ' es el mas grande';
+var cadenaCompleta = cadena + cadena2;
+console.log('La cadena completa es: ',cadenaCompleta);
+
+console.log('1-c:');
+var cad = 'hola', cad2 = 'mundo';
+var cantLetras = cad.length + cad2.length;
+console.log('La suma de las letras es: ',cantLetras);
